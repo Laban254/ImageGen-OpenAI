94980f2af1842c503733dad25ad896e059b23291
laban254 <labanrotich6544@gmail.com>
2024-03-09 14:36:37 +0300

    Add imagegen_app URLs and view for generating images

--- imagegen_app/views.py
+++ imagegen_app/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
 
-# Create your views here.
+from PIL import Image
+import requests
+from django.http import HttpResponse
+from django.shortcuts import render
+from openai import OpenAI
+
+client = OpenAI(api_key='sk-E......')
+
+
+def generate_image(request):
+    text_input = request.GET.get('text_input')
+    image_url = None
+    error_message = None
+    
+    if not text_input:
+        error_message = "Please provide input text."
+    else:
+        if '\n' in text_input:
+            prompt = text_input
+        else:
+            features_input = request.GET.get('features_input')
+            if features_input is not None:
+                prompt = f"{text_input}\n{features_input}"
+            else:
+                prompt = text_input
+            
+        response = client.images.generate(prompt=prompt, n=1, size='512x512')
+        image_url = response.data[0].url
+    
+    context = {'image_url': image_url, 'error_message': error_message}
+    return render(request, 'generate.html', context)
